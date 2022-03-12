c60e03b8d66417f8fcf9be546a33dc8a94767c9a
Filip Gregor <filip.gregor98@gmail.com>
2022-03-12 23:02:57 +0100

    Add print interpretation

--- src/vm.c
+++ src/vm.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+
+#include "include/constant.h"
 #include "include/vm.h"
 #include "include/bytecode.h"
 #include "include/memory.h"
@@ -37,7 +40,85 @@ void free_vm(vm_t* vm)
     free_chunk(vm->bytecode);
 }
 
+#define READ_BYTE_IP(vm) (*((vm)->ip++))
+#define READ_WORD_IP(vm) ((vm)->ip += 2, (*((vm)->ip - 2) << 8 | (*((vm)->ip - 1))))
+
+bool interpret_print(vm_t* vm, chunk_t* t) {
+    const char* str = AS_CSTRING(t->pool.data[READ_WORD_IP(vm)]);
+    uint8_t arg_count = READ_BYTE_IP(vm);
+    for(const char* ptr = str; *ptr != '\0'; ptr ++) {
+        if (*ptr == '~') {
+            constant_t val = pop(&vm->op_stack);
+            switch (val.type) {
+                case TYPE_INTEGER:
+                    printf("%d", AS_NUMBER(val));
+                    break;
+                case TYPE_NULL:
+                    printf("null");
+                    break;
+                case TYPE_BOOLEAN:
+                    printf(AS_BOOL(val) ? "true" : "false");
+                    break;
+                case TYPE_OBJECT:
+                default:
+                    fprintf(stderr, "Uknown value type to print.");
+                    return false;
+            }
+            arg_count -= 1;
+        } else {
+            putchar(*ptr);
+        }
+    }
+
+    if (arg_count != 0) {
+        fprintf(stderr, "Wrong number of arguments to print statement.");
+    }
+
+    return true;
+}
+
 interpret_result_t interpret(chunk_t* chunk)
 {
+    vm_t vm;
+    init_vm(&vm);
 
+    for(;;) {
+        switch(READ_BYTE_IP(&vm)) {
+            case OP_RETURN:
+                return INTERPRET_OK;
+            case OP_LABEL:
+                break;
+            case OP_DROP:
+                pop(&vm.op_stack);
+                break;
+            case OP_LITERAL: {
+                uint16_t index = READ_WORD_IP(&vm);
+                push(&vm.op_stack, vm.bytecode->pool.data[index]);
+            }
+            case OP_GET_LOCAL:
+            case OP_SET_LOCAL:
+            case OP_GET_GLOBAL:
+            case OP_SET_GLOBAL:
+            case OP_JUMP:
+            case OP_BRANCH:
+            case OP_OBJECT:
+            case OP_GET_FIELD:
+            case OP_SET_FIELD:
+            case OP_CALL_FUNCTION:
+            case OP_PRINT:
+                if (!interpret_print(&vm, chunk)) {
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                break;
+            case OP_ARRAY:
+            case OP_CALL_METHOD:
+            default:
+                fprintf(stderr, "Unknown instruction to interpret.");
+                return INTERPRET_RUNTIME_ERROR;
+
+        }
+    }
 }
+
+#undef READ_BYTE_IP
+#undef READ_WORD_IP
