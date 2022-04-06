731754a294558896cc84e26a02ab8e897cae3b82
Filip Gregor <filip.gregor98@gmail.com>
2022-04-06 13:25:11 +0200

    Remove build files

--- _build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gregofi1/skola/RUN/cfml
-# Build directory: /home/gregofi1/skola/RUN/cfml/_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
